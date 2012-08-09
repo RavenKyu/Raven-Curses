e3de9aba5ff69b456d32a2b7011f60b6c305178c
Raven <hong18s@gmail.com>
2012-08-10 05:06:34 +0900

    함수가 Low Level과 High Level 영역으로 나누었다. Low Level의 함수들이 가지는 인자들은 High Level까지 서로 상속 관계를 가질 수 있다.
    모든 화면 출력은 기본이 되는 함수인 put_a_character() 함수를 사용한다.
    
    아직은 완벽하진 않지만 외관상 문제가 없는 창을 손쉽게 출력 할 수 있는 API를 만들었다.
    대화상자를 출력하는 함수는 Low Level에서 만들어진  “채워진 박스 그리기” , “선 박스 그리기”, “그림자 그리” 등, Low Levl 함수의 이용과 상속 관계를 가지며 만들어 지게 된다.

--- MAIN.C
+++ MAIN.C
@@ -1,22 +1,42 @@
 #include "include/headers.h"
 
-/* #1 Functions for putting a text on screen  */
+/* #1 Low level functions for putting a text on screen  */
 void put_a_character(int y, int x, char character, int attribute);
-void put_a_string(int y, int x, char * string, int attribute);
+void put_a_string(int y, int x, char *string, int attribute);
 
-/* #2 Functions for making a box  */
+/* #2 Low level functions for making a box  */
+void make_a_filled_box(int y, int x, int width, int height, char option_flags);
+void make_a_shadow(int y, int x, int width, int height);
+void draw_a_quadrangle(int y, int x, int width, int height, int attribute, char option_flags);
 
-/* #3 Functions for filling the background with a extended ASCII */
+
+/* #3 Low level functions for filling the background with a extended ASCII */
 void fill_background(char tile, int attribute);
+
+/* #4 Low level functions for getting the point where you want */
+int get_the_center_point(int width_of_base, int length_of_object)
+{
+	return ((width_of_base - length_of_object) / 2);
+}
+
+
+/* #H1 High level functions for making a "Dialog Box"  */
+void make_a_dialog_box(int y, int x, int width, int height,
+	int attribute, char option_flags,
+	char *tilte, char dialog_flags);
 
 int main()
 {
 	clrscr();
 
-	fill_background(0xB0, TEXT_YELLOW);
+	fill_background(0xB0, BG_GRAY);
 
-	put_a_character(3, 3, 'A', TEXT_RED);
-	put_a_string(4, 3, "Hello World", TEXT_RED);
+	make_a_dialog_box(10, 30, 20, 5, TEXT_WHITE | BG_BLUE, 22, " Hello ", 1);
+	make_a_dialog_box(1, 1, 15, 5, TEXT_WHITE | BG_BLUE, 22, " Chat ", 1);
+
+	//put_a_character(3, 3, 'A', TEXT_RED);
+	//put_a_string(4, 3, "Hello World", TEXT_BLUE);
+	put_a_string(24, 1, "This is the first GUI program for me.", TEXT_YELLOW);
 
 	return 0;
 }
@@ -24,7 +44,6 @@ int main()
 /* #1 Functions for putting a text on screen  */
 void put_a_character(int y, int x, char character, int attribute)
 {
-
 	unsigned char far *location;	/* Point where you put a word */
 
 	location = (unsigned char far *)0xb8000000 + y * 160 + x * 2;
@@ -43,7 +62,7 @@ void put_a_string(int y, int x, char *string, int attribute)
 	strcpy(buffer, string);
 
 	/* Sending a word in buffer step over and over  */
-	for(i_count = 0; strlen(string) >= i_count; i_count++)
+	for(i_count = 0; strlen(string) - 1 >= i_count; i_count++)
 	{
 		put_a_character(y, x + i_count, buffer[i_count], attribute);
 	}
@@ -51,6 +70,66 @@ void put_a_string(int y, int x, char *string, int attribute)
 }
 
 /* #2 Functions for making a box  */
+void make_a_filled_box(int y, int x, int width, int height, char option_flags)
+{
+	int i_count_x;
+	int i_count_y;
+
+	for(i_count_y = 0; height >= i_count_y; i_count_y++)
+	{
+		for(i_count_x = 0; width >= i_count_x; i_count_x++)
+		{
+			put_a_character(y + i_count_y, x + i_count_x , 0xDB, TEXT_BLUE);
+		}
+	}
+	return;
+}
+
+void make_a_shadow(int y, int x, int width, int height)
+{
+	int i_count_x;
+	int i_count_y;
+
+	for(i_count_y = 0; height >= i_count_y; i_count_y++)
+	{
+		for(i_count_x = 0; width >= i_count_x; i_count_x++)
+		{
+			if((i_count_x == width) || (i_count_y == height))
+			{
+				put_a_character(y + i_count_y + 1, x + i_count_x + 1, 0xDB, BG_BLACK);
+			}
+		}
+	}
+
+	return;
+}
+
+void draw_a_quadrangle(int y, int x, int width, int height, int attribute, char option_flags)
+{
+	int i_count_x;
+	int i_count_y;
+
+	for(i_count_y = 0; height >= i_count_y; i_count_y++)
+	{
+		for(i_count_x = 0; width >= i_count_x; i_count_x++)
+		{
+			//if((y == i_count_y) || (y == i_count_x)
+			if((i_count_y == 0) || (i_count_y == height))
+			{
+				put_a_character(y + i_count_y, x + i_count_x, HORIZON_1_LINE, attribute);
+			}
+			else if((i_count_x == 0) || (i_count_x = width))
+			{
+				put_a_character(y + i_count_y, x + i_count_x, VERTICAL_1_LINE, attribute);
+			}
+		}
+	}
+	put_a_character(y, x, TOP_LEFT_1_LINE, attribute);
+	put_a_character(y, x + width, TOP_RIGHT_1_LINE, attribute);
+	put_a_character(y + height, x, BOTTOM_LEFT_1_LINE, attribute);
+	put_a_character(y + height, x + width, BOTTOM_RIGHT_1_LINE, attribute);
+
+}
 
 /* #3 Functions for filling the background with a extended ASCII */
 void fill_background(char tile, int attribute)
@@ -65,5 +144,22 @@ void fill_background(char tile, int attribute)
 			put_a_character(y, x, tile, attribute);
 		}
 	}
+	return;
+}
+
+/* High */
+void make_a_dialog_box(int y, int x, int width, int height,
+	int attribute, char option_flags,
+	char *title, char dialog_flags)
+{
+	int title_center;
+
+	make_a_filled_box(y, x, width, height, option_flags);
+	make_a_shadow(y, x, width, height);
+	draw_a_quadrangle(y, x, width, height, attribute, option_flags);
+
+	title_center = get_the_center_point(width, sizeof(title));
+	put_a_string(y, x - 2 + (title_center), title, TEXT_WHITE | BG_BLUE);
+
 	return;
 }
