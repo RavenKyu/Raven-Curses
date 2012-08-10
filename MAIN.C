7c62c9e81e9df77174ce94bfab5a2b698baa6757
Raven <hong18s@gmail.com>
2012-08-10 12:38:38 +0900

    상자들이 화면 이상의 크기를 가질 때, 자동으로 다음 줄로 이동하는 것을 막았다. 조건문이 알아보기 힘들게 짜여져 있다. 한번에 모두 처리 해 줄 코드는 없을까?

--- MAIN.C
+++ MAIN.C
@@ -5,8 +5,8 @@ void put_a_character(int y, int x, char character, int attribute);
 void put_a_string(int y, int x, char *string, int attribute);
 
 /* #2 Low level functions for making a box  */
-void make_a_filled_box(int y, int x, int width, int height, char option_flags);
-void make_a_shadow(int y, int x, int width, int height);
+void make_a_filled_box(int y, int x, int width, int height, int attribute, char option_flags);
+void make_a_shadow(int y, int x, int width, int height, int attribute);
 void draw_a_quadrangle(int y, int x, int width, int height, int attribute, char option_flags);
 
 
@@ -25,18 +25,37 @@ void make_a_dialog_box(int y, int x, int width, int height,
 	int attribute, char option_flags,
 	char *tilte, char dialog_flags);
 
-int main()
+
+/* online editor functions */
+void initialize(void)
 {
+	int i_count;
+
 	clrscr();
 
-	fill_background(0xB0, BG_GRAY);
+	/* Background */
+	fill_background(0xB0, BG_BLUE);
 
-	make_a_dialog_box(10, 30, 20, 5, TEXT_WHITE | BG_BLUE, 22, " Hello ", 1);
-	make_a_dialog_box(1, 1, 15, 5, TEXT_WHITE | BG_BLUE, 22, " Chat ", 1);
+	/* Top menu bar */
+	make_a_filled_box(0, 0, 79, 0, BG_GRAY, 0);
 
+
+	return;
+}
+
+int main()
+{
+	initialize();
+
+	make_a_dialog_box(9, 50, 50, 10, TEXT_WHITE | BG_BROWN, 22, " Hello ", 1);
+	make_a_dialog_box(-3, -3, 40, 8, TEXT_WHITE | BG_BLUE, 22, " Raven ", 1);
+	make_a_dialog_box(10, -3, 40, 8, TEXT_WHITE | BG_BLUE, 22, " Raven ", 1);
+
+	make_a_dialog_box(3, 15, 15, 5, TEXT_WHITE | BG_BLUE, 22, " Chat ", 1);
+	//make_a_dialog_box(4, 25, 30, 8, TEXT_WHITE | BG_RED, 22, "Raven", 1);
 	//put_a_character(3, 3, 'A', TEXT_RED);
 	//put_a_string(4, 3, "Hello World", TEXT_BLUE);
-	put_a_string(24, 1, "This is the first GUI program for me.", TEXT_YELLOW);
+	//put_a_string(24, 1, "This is the first GUI program for me.", TEXT_YELLOW);
 
 	return 0;
 }
@@ -70,7 +89,7 @@ void put_a_string(int y, int x, char *string, int attribute)
 }
 
 /* #2 Functions for making a box  */
-void make_a_filled_box(int y, int x, int width, int height, char option_flags)
+void make_a_filled_box(int y, int x, int width, int height, int attribute, char option_flags)
 {
 	int i_count_x;
 	int i_count_y;
@@ -79,13 +98,20 @@ void make_a_filled_box(int y, int x, int width, int height, char option_flags)
 	{
 		for(i_count_x = 0; width >= i_count_x; i_count_x++)
 		{
-			put_a_character(y + i_count_y, x + i_count_x , 0xDB, TEXT_BLUE);
+			if((24 < y + i_count_y) || (79 < x + i_count_x) || (0 > y + i_count_y) || (0 > x + i_count_x))
+			{
+				/* This code is preventing to draw when the box is out of screen  */
+			}
+			else
+			{
+				put_a_character(y + i_count_y, x + i_count_x , ' ', attribute);
+			}
 		}
 	}
 	return;
 }
 
-void make_a_shadow(int y, int x, int width, int height)
+void make_a_shadow(int y, int x, int width, int height, int attribute)
 {
 	int i_count_x;
 	int i_count_y;
@@ -96,7 +122,14 @@ void make_a_shadow(int y, int x, int width, int height)
 		{
 			if((i_count_x == width) || (i_count_y == height))
 			{
-				put_a_character(y + i_count_y + 1, x + i_count_x + 1, 0xDB, BG_BLACK);
+				if((x < 0) || (24 < y + i_count_y) || (79 < x + i_count_x) || (0 > y + i_count_y) || (0 > x + i_count_x))
+				{
+
+				}
+				else
+				{
+					put_a_character(y + i_count_y + 1, x + i_count_x + 1, ' ', BG_BLACK );
+				}
 			}
 		}
 	}
@@ -116,19 +149,47 @@ void draw_a_quadrangle(int y, int x, int width, int height, int attribute, char 
 			//if((y == i_count_y) || (y == i_count_x)
 			if((i_count_y == 0) || (i_count_y == height))
 			{
-				put_a_character(y + i_count_y, x + i_count_x, HORIZON_1_LINE, attribute);
+				if((24 < y + i_count_y) || (79 < x + i_count_x) || (0 > y + i_count_y) || (0 > x + i_count_x))
+				{
+
+				}
+				else
+				{
+					put_a_character(y + i_count_y, x + i_count_x, HORIZON_1_LINE, attribute);
+				}
 			}
 			else if((i_count_x == 0) || (i_count_x = width))
 			{
-				put_a_character(y + i_count_y, x + i_count_x, VERTICAL_1_LINE, attribute);
+				if((24 < y + i_count_y) || (79 < x + i_count_x) || (0 > y + i_count_y) || (0 > x + i_count_x))
+				{
+
+				}
+				else
+				{
+					put_a_character(y + i_count_y, x + i_count_x, VERTICAL_1_LINE, attribute);
+				}
 			}
 		}
 	}
-	put_a_character(y, x, TOP_LEFT_1_LINE, attribute);
-	put_a_character(y, x + width, TOP_RIGHT_1_LINE, attribute);
-	put_a_character(y + height, x, BOTTOM_LEFT_1_LINE, attribute);
-	put_a_character(y + height, x + width, BOTTOM_RIGHT_1_LINE, attribute);
 
+	if((79 > x) && (x > 0))
+	{
+		put_a_character(y, x, TOP_LEFT_1_LINE, attribute);
+	}
+
+	if((79 > x + i_count_x) || (0 < x + i_count_x))
+	{
+		put_a_character(y, x + width, TOP_RIGHT_1_LINE, attribute);
+	}
+	if((24 > y + height) && (0 < x))
+	{
+		put_a_character(y + height, x, BOTTOM_LEFT_1_LINE, attribute);
+	}
+	if((79 > x + width))
+	{
+		put_a_character(y + height, x + width, BOTTOM_RIGHT_1_LINE, attribute);
+	}
+	return;
 }
 
 /* #3 Functions for filling the background with a extended ASCII */
@@ -154,12 +215,21 @@ void make_a_dialog_box(int y, int x, int width, int height,
 {
 	int title_center;
 
-	make_a_filled_box(y, x, width, height, option_flags);
-	make_a_shadow(y, x, width, height);
+	/* Checking the minimum dialog box size */
+	if(26 > width) /* if not, make the size fixed as 26. */
+	{
+		width = 26;
+	}
+	if(8 > height)
+	{
+		height = 8;
+	}
+	make_a_filled_box(y, x, width, height, attribute, option_flags);
+	make_a_shadow(y, x, width, height, attribute);
 	draw_a_quadrangle(y, x, width, height, attribute, option_flags);
 
 	title_center = get_the_center_point(width, sizeof(title));
-	put_a_string(y, x - 2 + (title_center), title, TEXT_WHITE | BG_BLUE);
+	put_a_string(y, x - 2 + (title_center), title, attribute);
 
 	return;
 }
